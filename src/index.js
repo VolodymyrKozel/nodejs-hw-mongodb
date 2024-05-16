3c13426a7227caa5f87a761701ecd5123de894d3
Volodymyr <Volodumurvel@gmail.com>
2024-05-16 18:53:09 +0200

    mongo db conection

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-// src/index.js
-
+import { initMongoDB } from './db/initMongoDB.js';
 import { startServer } from './server.js';
 
-startServer();
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
+
+bootstrap();
