7309a5dc25ff477ba1170aee1b47a1ec23066b4c
Volodymyr <Volodumurvel@gmail.com>
2024-05-29 16:27:08 +0200

    Update auth.js

--- src/services/auth.js
+++ src/services/auth.js
@@ -18,6 +18,10 @@ const createSession = () => {
 };
 
 export const registerUser = async (payload) => {
+  const user = await UsersCollection.findOne({ email: payload.email });
+  if (user) {
+    throw createHttpError(409, 'Email in use!');
+  }
   const encryptedPassword = await bcrypt.hash(payload.password, 10);
 
   return await UsersCollection.create({
@@ -30,7 +34,7 @@ export const loginUser = async (payload) => {
   const user = await UsersCollection.findOne({ email: payload.email });
   console.log(user);
   if (!user) {
-    throw createHttpError(404, 'User not found!');
+    throw createHttpError(401, 'Unauthorized!');
   }
 
   const isMatch = await bcrypt.compare(payload.password, user.password);
