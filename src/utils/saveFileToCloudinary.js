f7ecafa0721041876589ca0969f005fe2fa18125
Volodymyr <Volodumurvel@gmail.com>
2024-06-16 20:37:06 +0200

    Update saveFileToCloudinary.js

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -1,12 +1,10 @@
 import cloudinary from 'cloudinary';
 
-import { env } from './env.js';
-
 cloudinary.v2.config({
   secure: true,
-  cloud_name: env('CLOUD_NAME'),
-  api_key: env('API_KEY'),
-  api_secret: env('API_SECRET'),
+  cloud_name: 'CLOUD_NAME',
+  api_key: 'API_KEY',
+  api_secret: 'API_SECRET',
 });
 
 export const saveFileToCloudinary = async (file) => {
