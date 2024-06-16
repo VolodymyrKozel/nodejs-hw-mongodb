4d4fe77614cc8f92a9f576c0c802c95f089094c0
Volodymyr <Volodumurvel@gmail.com>
2024-06-16 20:27:00 +0200

    cloud name change

--- src/utils/saveFileToUploadDir.js
+++ src/utils/saveFileToUploadDir.js
@@ -1,7 +1,7 @@
 import path from 'node:path';
 import fs from 'node:fs/promises';
 import { TEMPLATES_DIR, UPLOAD_DIR } from '../constants/index.js';
-import { env } from './env.js';
+import { env } from '../utils/env.js';
 
 export const saveFileToUploadDir = async (file) => {
   await fs.rename(
